86b7f956b56d4f2ebe15bce801e074893cd47104
Alex Segers <a.segers.dev@gmail.com>
2020-01-10 09:34:56 -0800

    fix(frontend > configureStore func): modify redux store to only use redux-logger in development

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,13 +5,18 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
 
-const configureStore = (preloadedState = {}) =>
-  createStore(
+const configureStore = (preloadedState = {}) => {
+  let middleware = [thunk];
+  if (process.env.NODE_ENV !== 'production') {
+    middleware = [...middleware, logger];
+  }
+  return createStore(
     rootReducer,
     preloadedState,
     composeWithDevTools(
-      applyMiddleware(thunk, logger)
+      applyMiddleware(...middleware)
     )
   );
+}
 
 export default configureStore;
