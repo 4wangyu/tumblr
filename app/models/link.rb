a258f22d1fa245daa1d10a73d14f7ab301293e6f
Alex Segers <a.segers.dev@gmail.com>
2020-03-16 23:32:41 -0700

    feature(link post): Add associations and validations
    - Add 'has_one' polymorphic association to map to corrsponding 'Post' model's content
    - Add 'has_one' association to map to corresponding 'User' model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,7 @@
 class Link < ApplicationRecord
+    # ----------------------------- Associations
+    has_one :post, as: :content, dependent: :destroy
+    has_one :user, through: :post
+    # ----------------------------- Validations
+    validates_presence_of :url, :title
 end
