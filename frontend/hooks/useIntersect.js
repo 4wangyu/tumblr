61cc89e10150852b4a685eb70c1229c1e3013335
Alex Segers <a.segers.dev@gmail.com>
2020-04-07 23:35:05 -0700

    Add custom hook, "useIntersect", which returns a IntersectionObserverEntry corresponding to the given ref

---
+++ frontend/hooks/useIntersect.js
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+
+const useIntersect = ({ root = null, rootMargin, threshold = 0 }) => {
+  const [entry, updateEntry] = useState({});
+  const [node, setNode] = useState(null);
+
+  const observer = useRef(
+    new window.IntersectionObserver(([entry]) => updateEntry(entry), {
+      root,
+      rootMargin,
+      threshold
+    })
+  );
+
+  useEffect(
+    () => {
+      const { current: currentObserver } = observer;
+      currentObserver.disconnect();
+
+      if (node) currentObserver.observe(node);
+
+      return () => currentObserver.disconnect();
+    },
+    [node]
+  );
+
+  return [setNode, entry];
+};
+
+useIntersect.buildThresholdArray = () => Array.from(Array(100).keys(), i => i / 100);
+
+export default useIntersect;
