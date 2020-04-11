d7cf7fb93cf957ee351a0772452ee8586cf69e32
Alex Segers <a.segers.dev@gmail.com>
2020-04-11 16:08:20 -0700

    Add custom hook "useOnClickOutside", which executes a callback when the client click outside of the referenced component

---
+++ frontend/hooks/useOnClickOutside.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+const useOnClickOutside = ({ ref, onClickOutside }) => {
+
+  useEffect(
+    () => {
+      const handleClickOutside = e => {
+        if (ref.current && !ref.current.contains(e.target)) onClickOutside();
+      };
+
+      document.addEventListener("mousedown", handleClickOutside);
+
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    },
+    [ref]
+  );
+
+};
+
+export default useOnClickOutside
