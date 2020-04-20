33925d77ee224fc0d2e1e9185747ddd155fed6c8
Alex Segers <a.segers.dev@gmail.com>
2020-04-19 20:56:56 -0700

    fix(TagsController): Remove before_action for a previously deleted method

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -1,9 +1,9 @@
 class Api::TagsController < ApplicationController
-  before_action :select_post
 
   def search 
     @tag_names = Tag.name_like(params[:query])
 
     render json: @tag_names
   end
+  
 end
